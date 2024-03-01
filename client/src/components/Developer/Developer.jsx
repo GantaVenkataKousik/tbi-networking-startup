537584140d29eb4c298ee6649cc1aa75cda0fbc7
GantaVenkatakousik <venkatakousikcse01@gmail.com>
2024-03-01 18:11:08 +0530

    commiting all the changes

--- client/src/components/Developer/Developer.jsx
+++ client/src/components/Developer/Developer.jsx
@@ -10,10 +10,10 @@ const Designer = () => {
                 <div >
                     <div className="details">
                         <div className="socialProfiles">
-                            <h3>Linked In : <span className="detail"><a href="">Venkatakousik</a></span></h3>
-                            <h3>Github : <span><a href="">GantaVenkataKousik</a></span></h3>
+                            <h3>Linked In : <span className="detail"><a href="https://www.linkedin.com/in/venkatakousik/">Venkatakousik</a></span></h3>
+                            <h3>Github : <span><a href="https://github.com/GantaVenkataKousik">GantaVenkataKousik</a></span></h3>
                             <h3>Contact : <span className="number">+91 7337066524</span></h3>
-                            <h3>Role : <span><a href="">Full Stack Developer</a></span></h3>
+                            <h3>Role : <span><a href="https://www.linkedin.com/in/venkatakousik/">Full Stack Developer</a></span></h3>
                         </div>
                         <img src="./persons/gvk.png" alt="" />
                     </div>
